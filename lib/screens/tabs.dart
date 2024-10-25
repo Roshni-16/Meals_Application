430873a20a377fce2a73b74d873769503baed14c
Roshni-16 <roshnibhilare16@gmail.com>
2024-10-25 11:10:50 +0530

    Working on the meals application which contains all the meals according to their categories, which is shown in the categories screen, next we also have the meals list where the meals are shown by the category been selected, when we choose a particular meal we have the list of ingredients and steps to make that item as well as the time that it will consume to get it done.

---
+++ lib/screens/tabs.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+import 'package:meals_application/models/meal.dart';
+
+import 'package:meals_application/screens/categories.dart';
+
+import 'package:meals_application/screens/meals.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
+  State<TabsScreen> createState() {
+    return _TabsScreenState();
+  }
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedPageIndex = 0;
+  final List<Meal> _favouriteMeals = [];
+
+  void _toggleMealFavouriteStatus(Meal meal) {
+    final isExisting = _favouriteMeals.contains(meal);
+
+    if (isExisting) {
+      _favouriteMeals.remove(meal);
+    } else {
+      _favouriteMeals.add(meal);
+    }
+  }
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget activePage = CategoriesScreen(onToggleFavourite: _toggleMealFavouriteStatus,);
+    var activePageTitle = 'Categories';
+
+    if (_selectedPageIndex == 1) {
+      activePage =MealsScreen(
+        meals: [], 
+        onToggleFavourite: _toggleMealFavouriteStatus,
+      );
+      activePageTitle = 'Your Favourites';
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(activePageTitle),
+      ),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        currentIndex: _selectedPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.set_meal),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favourites',
+          ),
+        ],
+      ),
+    );
+  }
+}
